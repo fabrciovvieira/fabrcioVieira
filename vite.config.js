0d027c41c22fc0c73e68c1d63f55db1d86ba2c91
fabrciovvieira <fabriciovvieira@gmail.com>
2024-05-29 23:12:09 -0300

    ajustes para deploy

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.js
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['framer-motion']
-    }
-  }
-}
-
+export default defineConfig({
+  plugins: [react()],
+})
