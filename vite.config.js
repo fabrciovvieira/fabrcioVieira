13e7179216ab29c1121118bf6626d12e791f48c3
fabrciovvieira <fabriciovvieira@gmail.com>
2024-05-29 22:46:39 -0300

    ajustes de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // vite.config.js
+// vite.config.js
 export default {
   build: {
     rollupOptions: {
-      external: ['@vercel/speed-insights/react']
+      external: ['@vercel/speed-insights/react', 'framer-motion']
     }
   }
 }
